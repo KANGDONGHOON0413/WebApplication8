07594140778d93d300719903b4d2794f827c361e
KANGDONGHOON0413 <kmerits@gmail.com>
2022-09-24 12:08:54 +0900

    초기 커밋.

---
+++ WebApplication8/DataContext/AspDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication8.Models;
+
+namespace WebApplication8.DataContext
+{
+    public class AspDbContext :DbContext
+    {
+    public DbSet<User> Users { get; set; }
+    public DbSet<Note> Notes { get; set; }
+
+       protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server = localhost; database = test; uid = sa; password = alencia");
+        }
+
+
+    }
+}
---
+++ WebApplication8/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication8.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserNo { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        [Required]
+        public string UserPw { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+    }
+}
